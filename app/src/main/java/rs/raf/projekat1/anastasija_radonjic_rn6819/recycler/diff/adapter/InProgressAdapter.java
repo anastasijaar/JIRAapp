597091540a43eef49b441d4cb87c437fab28431a
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2022-04-07 22:53:38 +0200

    Add files.

---
+++ app/src/main/java/rs/raf/projekat1/anastasija_radonjic_rn6819/recycler/diff/adapter/InProgressAdapter.java
@@ -0,0 +1,119 @@
+package rs.raf.projekat1.anastasija_radonjic_rn6819.recycler.diff.adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.function.Consumer;
+
+import rs.raf.projekat1.anastasija_radonjic_rn6819.R;
+import rs.raf.projekat1.anastasija_radonjic_rn6819.models.Ticket;
+
+public class InProgressAdapter extends ListAdapter<Ticket, InProgressAdapter.ViewHolder> {
+
+    public final Consumer<Ticket> onEnhancmentClicked;
+    public final Consumer<Ticket> onMoveRightTicket;
+    public final Consumer<Ticket> onMoveLeftTicket;
+
+    public InProgressAdapter(@NonNull DiffUtil.ItemCallback<Ticket> diffCallback, Consumer<Ticket> onEnhancmentClicked,
+                             Consumer<Ticket> onMoveRightTicket,
+                             Consumer<Ticket> onMoveLeftTicket) {
+            super(diffCallback);
+            this.onEnhancmentClicked = onEnhancmentClicked;
+            this.onMoveRightTicket = onMoveRightTicket;
+            this.onMoveLeftTicket = onMoveLeftTicket;
+            }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            // Create a new view
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.inprogress_list_item, parent, false);
+            return new ViewHolder(view, parent.getContext(), position -> {
+            Ticket ticket = getItem(position);
+            onEnhancmentClicked.accept(ticket);
+            },position ->{
+            Ticket ticket = getItem(position);
+            onMoveRightTicket.accept(ticket);
+            }, position ->{
+            Ticket ticket = getItem(position);
+            onMoveLeftTicket.accept(ticket);
+            });
+            }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            //hvatamo ticket koji treba trenutno da se prikaze
+            Ticket ticket = getItem(position);
+            holder.bind(ticket);
+            }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final Context context;
+
+        public ViewHolder(@NonNull View itemView, Context context,
+                          Consumer<Integer> onItemClicked,
+                          Consumer<Integer> onMoveTicket,
+                          Consumer<Integer> onDeleteTicket) {
+            super(itemView);
+            this.context = context;
+
+            itemView.setOnClickListener(v -> {
+                if (getBindingAdapterPosition() != RecyclerView.NO_POSITION)
+                    onItemClicked.accept(getBindingAdapterPosition());
+            });
+
+            itemView.findViewById(R.id.arrowRightInProgressIv).setOnClickListener(v -> {
+                if (getBindingAdapterPosition() != RecyclerView.NO_POSITION)
+                    onMoveTicket.accept(getBindingAdapterPosition());
+            });
+
+            itemView.findViewById(R.id.arrowLeftInProgressIv).setOnClickListener(v -> {
+                if (getBindingAdapterPosition() != RecyclerView.NO_POSITION)
+                    onDeleteTicket.accept(getBindingAdapterPosition());
+            });
+        }
+
+        public void bind(Ticket ticket){
+            SharedPreferences preferences = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+            Boolean isAdmin = preferences.getBoolean("isAdmin", false);
+    //            Boolean isInProgres = preferences.getBoolean("isInProgress", false);
+    //            Timber.e("IsInprogress je:" + isInProgres);
+            ((TextView)itemView.findViewById(R.id.titleInProgressTv)).setText(ticket.getTitle());
+            ((TextView)itemView.findViewById(R.id.descriptionInProgressTv)).setText(ticket.getDescription());
+
+            Drawable enc = ContextCompat.getDrawable(context, R.drawable.ic_enhancment);
+            Drawable bug = ContextCompat.getDrawable(context, R.drawable.ic_bug);
+            if(((TextView) itemView.findViewById(R.id.titleInProgressTv)).getText().toString().startsWith("En")){
+                ticket.setImage(enc);
+                ((ImageView)itemView.findViewById(R.id.rocketPictureInProgressIv)).setImageDrawable(enc);
+            }
+            else if(((TextView) itemView.findViewById(R.id.titleInProgressTv)).getText().toString().startsWith("Bu")){
+                ticket.setImage(enc);
+                ((ImageView)itemView.findViewById(R.id.rocketPictureInProgressIv)).setImageDrawable(bug);
+            }
+    //            if(!isInProgres && !isAdmin){
+    //                ((ImageView) itemView.findViewById(R.id.deletePictureIv)).setVisibility(View.VISIBLE);
+    //                ((ImageView) itemView.findViewById(R.id.arrowLeftIv)).setVisibility(View.GONE);
+    //
+    //            }else {
+    //                ((ImageView) itemView.findViewById(R.id.deletePictureIv)).setVisibility(View.GONE);
+    //                ((ImageView) itemView.findViewById(R.id.arrowLeftIv)).setVisibility(View.VISIBLE);
+    //            }
+        }
+
+    }
+
+}
